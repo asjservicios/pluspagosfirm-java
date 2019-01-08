516a5081725a90649541f1faee471486a1b58d5c
Lucía <lumarcolini@gmail.com>
2019-01-08 15:25:49 -0300

    SDKs seguridad del webform

---
+++ src/main/java/com/pluspago/SHA256Firm.java
@@ -0,0 +1,22 @@
+package com.pluspago;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256Firm {
+
+    public String getFirm(String ipClient, String guidComercio, String sucursalId, String monto) throws NoSuchAlgorithmException {
+
+        String input = (ipClient+"*"+ guidComercio+"*"+sucursalId+"*"+monto);
+        MessageDigest provider = MessageDigest.getInstance("SHA-256");
+        byte[] hashedBytes = provider.digest(input.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder output = new StringBuilder(2 * hashedBytes.length);
+        for (byte b : hashedBytes) {
+            output.append(String.format("%02X", b).toLowerCase());
+        }
+        return output.toString();
+    }
+}
+
