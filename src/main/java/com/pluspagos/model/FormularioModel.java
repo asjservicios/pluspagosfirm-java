7a98688615018ef9ea8a8c2db8e16f9317eb7d21
andres.ceccoli@waveapps.com.ar <andres.ceccoli@gmail.com>
2019-01-23 12:49:54 -0300

    Simplificado el envio de post en Java

--- src/main/java/com/pluspagos/model/FormularioModel.java
+++ src/main/java/com/pluspagos/model/FormularioModel.java
@@ -3,9 +3,11 @@ package com.pluspagos.model;
 import java.util.List;
 
 import com.pluspagos.AESEncrypter;
+import com.pluspagos.SHA256Firm;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class FormularioModel {
@@ -16,9 +18,10 @@ public class FormularioModel {
     private String comercio;
     private String sucursalComercio = "";
     private String transaccionComercioId;
-    private double monto;
+    private String monto;
     private List<String> productos = new ArrayList<String>();
     private String secretKey;
+    private String ip;
 
     public static class Builder {
         private FormularioModel pb = new FormularioModel();
@@ -31,40 +34,60 @@ public class FormularioModel {
             pb.hash = hash;
             return this;
         }
+
         public Builder callbackSuccess(String callbackSuccess) {
             pb.callbackSuccess = callbackSuccess;
             return this;
         }
+
         public Builder callbackCancel(String callbackCancel) {
             pb.callbackCancel = callbackCancel;
             return this;
         }
+
         public Builder comercio(String comercio) {
             pb.comercio = comercio;
             return this;
         }
+
         public Builder sucursal(String sucursal) {
             pb.sucursalComercio = sucursal;
             return this;
         }
+
         public Builder transaccionId(String transaccionId) {
             pb.transaccionComercioId = transaccionId;
             return this;
         }
+
         public Builder monto(double monto) {
-            pb.monto = monto;
+            pb.monto = Integer.toString((int) Math.round(monto * 100));
             return this;
         }
+
         public Builder addProducto(String producto) {
             pb.productos.add(producto);
             return this;
         }
+
+        public Builder ip(String ip) {
+            pb.ip = ip.trim();
+            return this;
+        }
+
         public FormularioModel build() {
+            if (pb.hash == null) {
+                try {
+                    pb.hash = new SHA256Firm().getFirm(pb.ip, pb.secretKey, pb.comercio, pb.sucursalComercio, pb.monto);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
             return pb;
         }
     }
 
-    public String toUrlEncodedForm() {
+    public byte[] toUrlEncodedForm() {
         char fieldSep = '&';
         char valueSep = '=';
 
@@ -78,12 +101,12 @@ public class FormularioModel {
                 result += "Producto[" + p + "]" + valueSep + URLEncoder.encode(productos.get(p), "utf-8") + fieldSep;
             }
 
-            result += "Monto" + valueSep + URLEncoder.encode(AESEncrypter.encryptString(Integer.toString((int) Math.round(monto * 100)), secretKey), "utf-8") + fieldSep;
+            result += "Monto" + valueSep + URLEncoder.encode(AESEncrypter.encryptString(monto, secretKey), "utf-8") + fieldSep;
             result += "SucursalComercio" + valueSep + URLEncoder.encode(AESEncrypter.encryptString(sucursalComercio, secretKey), "utf-8") + fieldSep;
             result += "CallbackCancel" + valueSep + URLEncoder.encode(AESEncrypter.encryptString(callbackCancel, secretKey), "utf-8") + fieldSep;
             result += "CallbackSuccess" + valueSep + URLEncoder.encode(AESEncrypter.encryptString(callbackSuccess, secretKey), "utf-8");
 
-            return result;
+            return result.getBytes("utf-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
             return null;
